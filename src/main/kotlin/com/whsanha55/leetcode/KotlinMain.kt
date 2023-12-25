c9ff2242340b2d1345e9f895c6410eb46d757ed2
jongha-hwang-switchwon <jjong@switchwon.com>
2023-12-25 20:37:56 +0900

    git init

---
+++ src/main/kotlin/com/whsanha55/leetcode/KotlinMain.kt
@@ -0,0 +1,10 @@
+package com.whsanha55.leetcode
+
+class KotlinMain {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello, world!")
+        }
+    }
+}
---
+++ src/main/kotlin/com/whsanha55/leetcode/Test.java
@@ -0,0 +1,8 @@
+package com.whsanha55.leetcode;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
